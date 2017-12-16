df64c78790a29d3caba1169b59b04b7071590280
roemhildtg <roemhildtg@gmail.com>
2017-12-16 11:00:54 -0600

    init

---
+++ src/app.js
@@ -0,0 +1,16 @@
+export class App {
+
+  configureRouter(config, router) {
+    config.title = 'Contacts';
+    config.map([
+      { route: '', moduleId: 'no-selection', title: 'Select'},
+      {route: 'contacts/:id', moduleId: 'contact-detail', name: 'contacts'}
+    ]);
+
+    this.router = router;
+  }
+
+  constructor() {
+    this.message = 'Hello World!';
+  }
+}
